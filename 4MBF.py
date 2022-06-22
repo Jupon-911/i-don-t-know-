b91643d4eb1f62a6de478d34acd120c25d4216ba
X　ツ A　X　E　L <103291633+Jupon-911@users.noreply.github.com>
2022-06-22 20:21:45 +0700

    Add files via upload

---
+++ 4MBF.py
@@ -0,0 +1,20 @@
+import os
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('mkdir /sdcard/4MBF-DATA')
+except:pass
+try:os.system('mkdir /sdcard/4MBF-DATA/OK')
+except:pass
+try:os.system('mkdir /sdcard/4MBF-DATA/CP')
+except:pass
+try:os.system('mkdir /sdcard/4MBF-DATA/TAP-A2F')
+except:pass
+try:os.system('touch .prox.txt')
+except:pass
+print("\n\x1b[1;92m Bypass By Krishna + Subhan! \033[1;37m")
+print("\n\x1b[1;92m Sorry For Bypass 😅 \033[1;37m")
+if __name__ == "__main__":
+        try:
+                __import__("aorec").login_lagi334()
+        except Exception as e:
+                exit(str(e))
